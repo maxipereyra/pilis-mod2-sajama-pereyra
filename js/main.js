ab1c86ba8cc3f015d94a61ba583f5f2232fa2b17
Fabián Abel Sajama <sajama230@gmail.com>
2022-08-24 22:07:19 -0300

    agrego formulario, estilos

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 //menu
 
-// registro 
+// registro
+let boton = document.getElementById("enviar");
+boton.addEventListener("click", onClick);
+
+function onClick (event) {
+    event.preventDefault();
+    let name = document.getElementById('nameComercio').value;
+    console.log(name);
+}
+
 
 // clima
