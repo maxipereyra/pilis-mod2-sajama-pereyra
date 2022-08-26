3d95497acc62634ab264a563e772325582abcc76
Fabián Abel Sajama <sajama230@gmail.com>
2022-08-25 22:48:37 -0300

    agrego evento, estilos, librerias lightgallery

--- js/main.js
+++ js/main.js
@@ -1,13 +1,44 @@
-//menu
 
 // registro
 let boton = document.getElementById("enviar");
 boton.addEventListener("click", onClick);
 
-function onClick (event) {
+function onClick(event) {
     event.preventDefault();
-    let name = document.getElementById('nameComercio').value;
-    console.log(name);
+    const mensaje = {
+        comercio: document.getElementById('nameComercio').value,
+        titular: document.getElementById('nameTitular').value,
+        celular: document.getElementById('celular').value,
+    }
+
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        body: JSON.stringify(mensaje),
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+
+    })
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+            Swal.fire({
+                position: 'top-center',
+                icon: 'success',
+                // iconColor: '#d62828',
+                iconColor: '#f77f00',
+                title: 'Registro Exitoso',
+                showConfirmButton: false,
+                background : '#003049',
+                color : '#fff',
+                timer: 1500
+            });
+            cleanForm();
+        })
+
+}
+
+function cleanForm() {
+    let formulario = document.getElementById('formulario');
+    formulario.reset();
 }
 
 
